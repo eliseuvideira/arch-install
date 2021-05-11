f999b4ad2ba4780ed836d367f3f5f48c5465ab28
Eliseu Videira <eliseuvideira1@gmail.com>
2021-05-11 08:57:59 -0300

    :sparkles: improved partitioning

--- arch.sh
+++ arch.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-SWAP_SIZE="$1"
-
 ping -c 3 archlinux.org
 timedatectl set-ntp true
 timedatectl status
-sgdisk -n 1:0:+500M -t 1:ef00 -c 1:boot /dev/sda
-sgdisk -n 2::+"$SWAP_SIZE" -t 2:8200 -c 2:swap /dev/sda
-sgdisk -n 3:: -t 3:8300 -c 3:root /dev/sda
+sgdisk -n 1:0:+512MiB  -t 1:ef00  -c 1:efi   /dev/sda
+sgdisk -n 2::+256MiB   -t 2:8300  -c 2:boot  /dev/sda
+sgdisk -n 3::+4GiB     -t 3:8200  -c 3:swap  /dev/sda
+sgdisk -n 4::          -t 4:8300  -c 4:root  /dev/sda
 sgdisk -p /dev/sda
 mkfs.fat -F32 /dev/sda1
-mkswap /dev/sda2
-swapon /dev/sda2
-mkfs.ext4 /dev/sda3
+mkfs.ext4 /dev/sd2
+mkswap /dev/sda3
+swapon /dev/sda3
+mkfs.ext4 /dev/sda4
 mount /dev/sda3 /mnt
 mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
