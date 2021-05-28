f3236d3cd159349b71f4db7422bd253173e2fa10
Eliseu Videira <eliseuvideira1@gmail.com>
2021-05-28 00:03:53 +0000

    :wrench: fixed arch install

--- arch.sh
+++ arch.sh
@@ -1,50 +1,76 @@
-#!/bin/sh
 
-check_internet() {
+has_internet() {
   if ping -q -c 1 -W 1 archlinux.org >/dev/null; then
     return 0;
   fi
   return 1;
 }
 
+setup_date() {
+  timedatectl set-ntp true
+}
+
 write_partition_table() {
   boot_size="$1"
   swap_size="$2"
   device="$3"
+  wipefs -a "${device}*"
   printf ",$boot_size,\n,$swap_size,82\n,," | sfdisk "$device" --label dos
 }
 
 set_ext2() {
-  partition="$1"
-  mkfs.ext2 "$partition"
+  mkfs.ext2 "$1"
 }
 
 set_swap() {
-  mkswap "$partition"
-  swapon "$partition"
+  mkswap "$1"
+  swapon "$1"
 }
 
 set_ext4() {
-  partition="$1"
-  mkfs.ext4 "$partition"
+  mkfs.ext4 "$1"
 }
 
+mount_partitions() {
+  boot_partition="$1"
+  root_partition="$2"
+  mount_point="$3"
+  mkdir -p "$3"
+  mount "$root_partition" "$mount_point"
+  mkdir /mnt/arch/boot
+  mount "$boot_partition" "${mount_point}/boot"
+}
+
+get_packages() {
+  cat <<EOF
+base
+base-devel
+linux
+linux-firmware
+zsh
+neovim
+networkmanager
+grub
+EOF
+}
+
+if ! has_internet; then
+  echo "no internet available"
+  exit 1
+fi
+
+setup_date
+
 write_partition_table 512M 4G /dev/sda
+
 set_ext2 /dev/sda1
 set_swap /dev/sda2
 set_ext4 /dev/sda3
 
-ping -c 3 archlinux.org
-timedatectl set-ntp true
-timedatectl status
-mkfs.fat -F32 /dev/sda1
-mkfs.ext4 /dev/sd2
-mkswap /dev/sda3
-swapon /dev/sda3
-mkfs.ext4 /dev/sda4
-mount /dev/sda3 /mnt
-mkdir /mnt/boot
-mount /dev/sda1 /mnt/boot
-pacstrap /mnt base base-devel linux linux-firmware zsh neovim
-genfstab -U /mnt >>/mnt/etc/fstab
-arch-chroot /mnt
+mount_partitions /dev/sda1 /dev/sda3 /mnt/arch
+
+pacstrap /mnt/arch $(get_packages)
+
+genfstab -U /mnt/arch >>/mnt/arch/etc/fstab
+
+echo "arch-chroot /mnt/arch"
